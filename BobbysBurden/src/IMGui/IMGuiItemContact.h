ec4adb60e7a382ba4f6e8ddf04f82aee3585b225
dogstar25 <33813377+dogstar25@users.noreply.github.com>
2023-09-16 18:32:49 -0400

    mouse cursor and take action done

---
+++ BobbysBurden/src/IMGui/IMGuiItemContact.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "MobyDick.h"
+
+class IMGuiItemContact : public IMGuiItem
+{
+public:
+	IMGuiItemContact() = default;
+	IMGuiItemContact(std::string gameObjectType, b2Vec2 padding, ImVec4 backgroundColor, ImVec4 textColor,
+		ImVec4 buttonColor, ImVec4 buttonHoverColor, ImVec4 buttonActiveColor, bool autoSize);
+	glm::vec2 render() override;
+
+private:
+
+	std::string m_label;
+
+	void _buildActionRow(GameObject* interfacedObject);
+
+	
+
+};
